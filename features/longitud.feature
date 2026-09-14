af43e26a83155f6ad6a0a00d424cbc121369306e
Martinvivas <129123814+Martinvivas@users.noreply.github.com>
2026-09-14 20:37:26 -0300

    RED: test_representa_palabra_de_diferente_longitud

--- features/longitud.feature
+++ features/longitud.feature
@@ -8,5 +8,5 @@ Característica: Ver longitud de la palabra
 @CA-01
   Escenario: Inicio del juego con palabra enmascarada
     Dado una partida con la palabra "GATO"
-    Entonces se ve la palabra "____"
+    Entonces se ve la palabra "_ _ _ _ "
 
--- test/ahorcado_test.py
+++ test/ahorcado_test.py
@@ -3,4 +3,10 @@ from src.ahorcado import Ahorcado
 
 def test_representa_palabra_inicial_con_guiones_bajos():
     juego = Ahorcado("GATO")
-    assert juego.palabra_enmascarada() == "_ _ _ _"
+    assert juego.palabra_enmascarada() == "_ _ _ _ "
+
+def test_representa_palabra_de_diferente_longitud():
+    juego = Ahorcado("PERRO")
+    assert juego.palabra_enmascarada() == "_ _ _ _ _"
+
+    
